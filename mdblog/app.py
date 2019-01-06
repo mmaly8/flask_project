693271fcaf7fbca64a057c1071a38876c9340cc4
mmaly <malymatej8@gmail.com>
2019-01-06 17:38:23 +0000

    Initial application

---
+++ mdblog/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+
+flask_app = Flask(__name__)
+
+@flask_app.route("/")
+def view_welcome_page():
+    return render_template("welcome_page.jinja", text="takto vies menit text")
+
+@flask_app.route("/about/")
+def view_about():
+    return render_template("about.jinja")
+
+"""
+@flask_app.route("/admin/")
+def view_admin():
+    return "Hello Admin"
+
+@flask_app.route("/admin/<string:name>/")
+def view_admin_name(name):
+    return "Hello {}".format(name)
+"""
